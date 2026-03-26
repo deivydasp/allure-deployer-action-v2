171fee66a439956826945972edcb22604bf8866a
Deivydas Piliukaitis <deivydas.piliukaitis@visma.com>
2026-03-26 09:36:47 +0200

    Test logo align=middle with spacing

--- src/features/messaging/github-notifier.ts
+++ src/features/messaging/github-notifier.ts
@@ -24,7 +24,7 @@ export class GitHubNotifier implements Notifier {
     async notify(data: NotificationData): Promise<void> {
         const { passed, failed, broken, skipped, unknown } = data.resultStatus;
 
-        const logo = `<img src="https://raw.githubusercontent.com/deivydasp/allure-deployer-action-v2/master/assets/allure-logo.svg" width="20" height="20" alt="Allure" align="top">`;
+        const logo = `<img src="https://raw.githubusercontent.com/deivydasp/allure-deployer-action-v2/master/assets/allure-logo.svg" width="20" height="20" alt="Allure" align="middle">`;
         let message = '';
         if (data.reportUrl) {
             message += `${logo}&nbsp;**Test Report**: [${data.reportUrl}](${data.reportUrl})\n`;
