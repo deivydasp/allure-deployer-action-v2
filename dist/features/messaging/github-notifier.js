e103ac0263cdb7ff45be7e138a9d3c06f8dd85fc
Deivydas Piliukaitis <deivydas.piliukaitis@visma.com>
2026-03-26 09:37:17 +0200

    Test logo align=baseline with spacing

--- dist/features/messaging/github-notifier.js
+++ dist/features/messaging/github-notifier.js
@@ -7,7 +7,7 @@ export class GitHubNotifier {
     }
     async notify(data) {
         const { passed, failed, broken, skipped, unknown } = data.resultStatus;
-        const logo = `<img src="https://raw.githubusercontent.com/deivydasp/allure-deployer-action-v2/master/assets/allure-logo.svg" width="20" height="20" alt="Allure" align="middle">`;
+        const logo = `<img src="https://raw.githubusercontent.com/deivydasp/allure-deployer-action-v2/master/assets/allure-logo.svg" width="20" height="20" alt="Allure" align="baseline">`;
         let message = '';
         if (data.reportUrl) {
             message += `${logo}&nbsp;**Test Report**: [${data.reportUrl}](${data.reportUrl})\n`;
